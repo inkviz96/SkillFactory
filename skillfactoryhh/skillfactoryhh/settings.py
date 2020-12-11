6fc0f168842855aca9b54cbd09d220c0f24aec3d
Semyon Berezovsky <saint96bsemenb@gmail.com>
2020-12-11 15:55:39 +0300

    settings change

--- skillfactoryhh/skillfactoryhh/settings.py
+++ skillfactoryhh/skillfactoryhh/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'password_manage',
+    'registration',
+    'registration_students',
 ]
 
 MIDDLEWARE = [
