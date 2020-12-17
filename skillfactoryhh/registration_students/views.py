9c02fb6b1bda84aefb1d15f853464eff77bfa433
eternityxxxx <gpavlikov7@gmail.com>
2020-12-17 19:20:29 +0600

    Add registration view

--- skillfactoryhh/registration_students/views.py
+++ skillfactoryhh/registration_students/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from .serializers import RegistrationSerializer
+
+
+User = get_user_model()
+
+
+class RegistrationAPIView(APIView):
+
+    serializer_class = RegistrationSerializer
+
+    def post(self, request):
+
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
