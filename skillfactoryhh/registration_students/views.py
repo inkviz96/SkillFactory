bb501b88948a84cab39d6a5d5cdfd51122b652bf
eternityxxxx <gpavlikov7@gmail.com>
2020-12-17 22:10:08 +0600

    Add login view

--- skillfactoryhh/registration_students/views.py
+++ skillfactoryhh/registration_students/views.py
@@ -1,10 +1,9 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate, login
 
-from .serializers import RegistrationSerializer
-
+from .serializers import RegistrationSerializer, LoginSerializer
 
 User = get_user_model()
 
@@ -20,3 +19,18 @@ class RegistrationAPIView(APIView):
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class LoginAPIView(APIView):
+
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = authenticate(username=request.data['email'], password=request.data['password'])
+        login(request, user)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
