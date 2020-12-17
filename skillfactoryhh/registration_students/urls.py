fe7b8c637fe106906d6c5d6460c769c7a7f07e49
eternityxxxx <gpavlikov7@gmail.com>
2020-12-17 22:10:22 +0600

    Add login url

--- skillfactoryhh/registration_students/urls.py
+++ skillfactoryhh/registration_students/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import RegistrationAPIView
+from .views import RegistrationAPIView, LoginAPIView
 
 
 urlpatterns = [
     path('api/registration/', RegistrationAPIView.as_view(), name='registration'),
+    path('api/login/', LoginAPIView.as_view(), name='login'),
 ]
