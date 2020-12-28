22ace09d8d8c68bc78b4a42c854098072db0fb00
AlenaPliusnina <64252655+AlenaPliusnina@users.noreply.github.com>
2020-12-29 00:27:33 +0300

    deleted unnecessary urls

--- skillfactoryhh/skillfactoryhh/urls.py
+++ skillfactoryhh/skillfactoryhh/urls.py
@@ -19,11 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # path('api-auth/', include('rest_framework.urls')),
-    # path('rest-auth/', include('rest_auth.urls')),
-    # path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    # path('api/', include('registration.urls', namespace='api')),
-
     # path to djoser end points
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
