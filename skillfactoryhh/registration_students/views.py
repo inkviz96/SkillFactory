625e6069f170a607d5db02e965e595683fd6d03b
eternityxxxx <gpavlikov7@gmail.com>
2020-12-28 23:28:02 +0600

    Edit registration view

--- skillfactoryhh/registration_students/views.py
+++ skillfactoryhh/registration_students/views.py
@@ -1,22 +1,40 @@
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.contrib.sites.shortcuts import get_current_site
 from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.contrib.auth import get_user_model, authenticate, login
+from rest_framework.authtoken.models import Token
 
-from .serializers import RegistrationSerializer, LoginSerializer
-
-User = get_user_model()
+from .serializers import RegistrationSerializer
+from .models import User
 
 
 class RegistrationAPIView(APIView):
 
     serializer_class = RegistrationSerializer
+    permission_classes = (AllowAny,)
 
     def post(self, request):
 
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save()
+        user = serializer.save()
+
+        token = Token.objects.get(user=user).key
+
+        serializer.data['token'] = token
+
+        email = EmailMessage(
+            'Подтверждение регистрации',
+            f'{get_current_site(request)}/api/confirm_account/?token={token}',
+            settings.EMAIL_HOST_USER,
+            [serializer.data['email'], ],
+        )
+        email.fail_silently = False
+        email.send()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
