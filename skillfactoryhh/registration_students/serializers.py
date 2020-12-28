2fd4237dc313524546b04680eb47976f5c9b826f
eternityxxxx <gpavlikov7@gmail.com>
2020-12-28 23:18:57 +0600

    Edit reg serializer, delete login serializer

--- skillfactoryhh/registration_students/serializers.py
+++ skillfactoryhh/registration_students/serializers.py
@@ -1,9 +1,7 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
-from django.contrib.auth import get_user_model, authenticate
 
-
-User = get_user_model()
+from .models import User
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
@@ -43,51 +41,12 @@ class RegistrationSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
-        return User.objects.create_user(
+
+        user = User.objects.create_user(
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
-            username=validated_data['email'],
             email=validated_data['email'],
             password=validated_data['password'],
         )
 
-
-class LoginSerializer(serializers.ModelSerializer):
-
-    email = serializers.EmailField(
-        write_only=True,
-        required=True,
-    )
-
-    password = serializers.CharField(
-        write_only=True,
-        required=True,
-    )
-
-    class Meta:
-
-        model = User
-        fields = [
-            'id',
-            'email',
-            'password',
-        ]
-
-    def validate(self, data):
-
-        email = data.get('email', None)
-        password = data.get('password', None)
-
-        user = authenticate(username=email, password=password)
-
-        if user is None:
-            raise serializers.ValidationError(
-                'Пользователь с таким именем и паролем не найден',
-            )
-
-        if not user.is_active:
-            raise serializers.ValidationError(
-                'Данный пользователь не активен',
-            )
-
-        return super().validate(self)
+        return user
