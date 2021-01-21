4b7415d7c52b490d129e5203ee99ef24074c9e0b
eternityxxxx <gpavlikov7@gmail.com>
2021-01-21 23:37:15 +0600

    Update models

--- skillfactoryhh/registration_students/models.py
+++ skillfactoryhh/registration_students/models.py
@@ -1,28 +1,14 @@
+from datetime import datetime, timedelta
+from time import mktime
+from jwt import encode
 from django.conf import settings
-from django.dispatch import receiver
 from django.db import models
-from django.db.models.signals import post_save
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, first_name, last_name, email, password=None):
-
-        user = self.model(
-            first_name=first_name,
-            last_name=last_name,
-            email=self.normalize_email(email),
-        )
-
-        user.set_password(password)
-
-        user.save(using=self._db)
-
-        return user
 
     def create_superuser(self, email, password):
-
         user = self.model(
             email=self.normalize_email(email),
         )
@@ -36,29 +22,59 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def get_by_natural_key(self, email):
 
-class User(AbstractBaseUser):
+        return self.get(email=email)
 
-    first_name = models.CharField(max_length=128, default='')
-    last_name = models.CharField(max_length=128, default='')
-    email = models.EmailField(unique=True)
-    is_active = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
+
+class StudentManager(BaseUserManager):
+
+    def create_student(self, first_name, last_name, email, password=None):
+        if email is None:
+            raise TypeError('Users must have an email address.')
+
+        student = Student(
+            first_name=first_name,
+            last_name=last_name,
+            email=self.normalize_email(email),
+        )
+        student.set_password(password)
+        student.save()
+
+        return student
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(db_index=True, max_length=30, unique=True)
+
+    is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
 
     objects = UserManager()
 
-    def has_perm(self, perm, obj=None):
-        return self.is_superuser
+    @property
+    def token(self):
+        dt = datetime.now() + timedelta(days=1)
 
-    def has_module_perms(self, app_label):
-        return self.is_superuser
+        token = encode({
+            'id': self.pk,
+            'exp': int(mktime(dt.timetuple()))
+        }, settings.SECRET_KEY, algorithm='HS256')
+
+        return token
+
+    def __str__(self):
+        return self.email
 
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
+class Student(User, PermissionsMixin):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    
+    objects = StudentManager()
