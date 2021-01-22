ff3efe0ae4a2aa6501feb970577fc8e7d42572cb
AlenaPliusnina <a564352@gmail.com>
2021-01-22 17:15:05 +0300

    added new urls for company login/registration

--- skillfactoryhh/registration/urls.py
+++ skillfactoryhh/registration/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
 
-from .views import CompanyProfileListCreateView, CompanyProfileDetailView
-
+from .views import CompanyProfileListCreateView, CompanyProfileDetailView, RegistrationCompanyAPIView, \
+    LoginCompanyAPIView
 
 urlpatterns = [
+    path('companies/registration/', RegistrationCompanyAPIView.as_view()),
+    path('companies/login/', LoginCompanyAPIView.as_view()),
     # gets all companies profiles and create a new profile
     path("all-companies-profiles", CompanyProfileListCreateView.as_view(), name="all-companies-profiles"),
     # retrieves profile details of the currently logged in user
