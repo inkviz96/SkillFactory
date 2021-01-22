4b764995a61975c50fcc38bba79e913eca47618d
AlenaPliusnina <a564352@gmail.com>
2021-01-22 17:43:38 +0300

    add  CompanyProfileSerializer and LoginCompanySerializer

--- skillfactoryhh/registration/serializers.py
+++ skillfactoryhh/registration/serializers.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import authenticate
 from rest_framework import serializers
 from .models import CompanyProfile
+from .models import Company
 
 
 class CompanyProfileSerializer(serializers.ModelSerializer):
@@ -8,3 +10,61 @@ class CompanyProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = CompanyProfile
         fields = '__all__'
+
+
+class RegistrationCompanySerializer(serializers.ModelSerializer):
+    """ Сериализация регистрации пользователя и создания нового. """
+
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True
+    )
+
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    class Meta:
+        model = Company
+
+        fields = ['email', 'password', 'token']
+
+    def create(self, validated_data):
+
+        return Company.objects.create_company(**validated_data)
+
+
+class LoginCompanySerializer(serializers.Serializer):
+    email = serializers.CharField(max_length=255)
+    password = serializers.CharField(max_length=128, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        if email is None:
+            raise serializers.ValidationError(
+                'An email address is required to log in.'
+            )
+
+        if password is None:
+            raise serializers.ValidationError(
+                'A password is required to log in.'
+            )
+
+        user = authenticate(username=email, password=password)
+
+        if user is None:
+            raise serializers.ValidationError(
+                'A user with this email and password was not found.'
+            )
+
+        if not user.is_active:
+            raise serializers.ValidationError(
+                'This user has been deactivated.'
+            )
+
+        return {
+            'email': user.email,
+            'token': user.token
+        }
