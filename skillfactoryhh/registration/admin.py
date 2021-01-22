d3723935e86c90583b37a59487818fa9be5a2df4
AlenaPliusnina <a564352@gmail.com>
2021-01-22 17:12:50 +0300

    add company model to admin panel

--- skillfactoryhh/registration/admin.py
+++ skillfactoryhh/registration/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import CompanyProfile
+from .models import CompanyProfile, Company
 
 
 @admin.register(CompanyProfile)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Company)
 class AuthorAdmin(admin.ModelAdmin):
     pass
 
