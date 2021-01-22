8f8c149e1947722c6c444216f13a885afd8ca618
AlenaPliusnina <a564352@gmail.com>
2021-01-22 17:09:53 +0300

    update registration company api urls

--- skillfactoryhh/skillfactoryhh/urls.py
+++ skillfactoryhh/skillfactoryhh/urls.py
@@ -19,10 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # path to djoser end points
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
-
     # path to registration's app endpoints
-    path("api/registration/", include("registration.urls")),
+    path("api/", include("registration.urls")),
 ]
